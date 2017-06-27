700c669f65e43a96c99f58de2368df9d2b8558c9
devlaf <devin@thelevelup.com>
2017-06-26 21:16:10 -0400

    Add some postgres stuff

---
+++ src/Carver/DataStore/DBExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Carver
+{
+    /// <summary>
+    /// An exception that represents errors connecting to a remote database.  Likely indicates an error with 
+    /// the connection string.
+    /// </summary>
+    public class DatabaseConnectionException : Exception
+    {
+        public DatabaseConnectionException(string message)
+            : base(message) { }
+        public DatabaseConnectionException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
---
+++ src/Carver/DataStore/PostgresDBConnection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using log4net;
+using Npgsql;
+using Dapper;
+
+namespace Carver
+{
+    internal class PostgresDBConnection
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(PostgresDBConnection));
+
+        /// <summary> On-demand postgres connection </summary>
+        /// <remarks> This value is not cached in memory; we'll allow a new connection to be made for each request
+        /// and let Postgres manage connection pooling.  This makes threading etc. simpler here, and allows for a
+        /// dynamic response to config updates. </remarks>
+        public IDbConnection Connection => new NpgsqlConnection(BuildConnectionString());
+
+        private static string BuildConnectionString()
+        {
+            var connectionOptions = new Dictionary<string, object>
+            {
+                { "Server",                 Configuration.GetValue<string>("db_server",                  "127.0.0.1" )},
+                { "Port",                   Configuration.GetValue<int>   ("db_port",                    5432        )},
+                { "Database",               Configuration.GetValue<string>("db_name",                    "carver_db" )},
+                { "UserId",                 Configuration.GetValue<string>("db_username",                ""          )},
+                { "Password",               Configuration.GetValue<string>("db_password",                ""          )},
+                { "CommandTimeout",         Configuration.GetValue<int>   ("db_command_timeout_sec",     20          )},
+                { "SslMode",                Configuration.GetValue<string>("db_ssl_mode",                "Require"   )},
+                { "Pooling",                Configuration.GetValue<bool>  ("db_pooling",                 true        )},
+                { "MinPoolSize",            Configuration.GetValue<int>   ("db_min_pool_size",           1           )},
+                { "MaxPoolSize",            Configuration.GetValue<int>   ("db_max_pool_size",           20          )},
+                { "ConnectionIdleLifeTime", Configuration.GetValue<int>   ("db_connection_lifetime_sec", 15          )}
+            };
+
+            return string.Join("", connectionOptions.Select(x => string.Format("{0}={1};", x.Key, x.Value)));
+        }
+
+        public static void TestConnection(IDbConnection con)
+        {
+            try
+            {
+                con.Execute("SELECT 1");
+            }
+            catch (NpgsqlException ex)
+            {
+                const string errorMessage = "Could not establish a connection to the Postgres database.  Check " +
+                                            "the connection settings.";
+                Log.ErrorFormat("{0}: {1}{2}", errorMessage, Environment.NewLine, ex);
+                throw new DatabaseConnectionException(errorMessage, ex);
+            }
+        }
+    }
+}
