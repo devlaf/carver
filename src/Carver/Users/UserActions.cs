60371257c294aa5b763f0b1af244a2daeae88855
devlaf <devin@thelevelup.com>
2017-06-27 17:38:50 -0400

    Add user authentication

---
+++ src/Carver/Users/UserActions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Carver.DataStore;
+using log4net;
+
+namespace Carver.Users
+{
+    internal static class UserActions
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(UserActions));
+
+        internal static async Task<long> CreateNewUser(string username, string password, string email, UserGroup role)
+        {
+            ValidateEmail(email);
+            var userIdentity = await UserAuthentication.GenerateUserCreds(username, password);
+
+            Log.Info(string.Format("Creating new user [username={0} email={1}, role={2}", username, email,
+                Enum.GetName(typeof(UserGroup), role)));
+
+            IUserStore userStore = new PostgresDataStore();
+            return await userStore.CreateUser(new User(username, email, userIdentity.HashedPassword, 
+                userIdentity.Salt, userIdentity.HashingIterations, role, DateTime.Now, true));
+        }
+
+        public static async Task UpdateUserPassword(string username, string oldPassword, string newPassword)
+        {
+            if (!await ValidateUser(username, oldPassword))
+                throw new ArgumentException("Specified username/oldPassword is not correct.");
+
+            var newUserIdentity = await UserAuthentication.GenerateUserCreds(username, newPassword);
+            Log.Info(string.Format("Updating password for user " + username));
+
+            IUserStore userStore = new PostgresDataStore();
+            var currentUser = (await userStore.GetUser(username)).Value;
+            var currentUserId = (await userStore.GetUserId(username)).Value;
+
+            await userStore.UpdateUser(currentUserId, new User(currentUser.Username, currentUser.Email, 
+                newUserIdentity.HashedPassword, newUserIdentity.Salt, newUserIdentity.HashingIterations, 
+                currentUser.UserGroup, currentUser.CreationDate, currentUser.Active));
+        }
+
+        public static async Task<bool> ValidateUser(string username, string password)
+        {
+            IUserStore userStore = new PostgresDataStore();
+
+            var user = await userStore.GetUser(username);
+
+            if (user == null)
+                return false;
+
+            if (!user.Value.Active)
+                return false;
+
+            var userCreds = new UserAuthentication.UserCredentials(user.Value.HashedPassword, 
+                user.Value.Salt, user.Value.HashingIterations);
+
+            return await (UserAuthentication.ConfirmUserPassword(userCreds, password));
+        }
+
+        private static void ValidateEmail(string emailAddress)
+        {
+            if (!emailAddress.Contains("@"))  // Clearly I'm being very thorough.
+                throw new InvalidEmailException("The provided email did not specify a domain.");
+        }
+
+        internal class InvalidEmailException : Exception
+        {
+            public InvalidEmailException(string message) 
+                : base(message) { }
+            public InvalidEmailException(string message, Exception innerException) 
+                : base(message, innerException) { }
+        }
+    }
+}
---
+++ src/Carver/Users/UserAuthentication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carver.Users
+{
+    /// <summary>
+    /// A collection of static methods for managing passwords, roles, etc.
+    /// </summary>
+    /// <remarks>
+    /// Strategy: PBKDF2 and 64 byte salt.  
+    /// 
+    /// PBKDF2 justification: 
+    /// .NET framework's built-in PBKDF2 implementation has been cryptographically 
+    /// verified, whereas none of the freely available .net implementations of bycrypt 
+    /// have been.  Also, it is the NIST reccomendation.
+    /// see http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-132.pdf
+    /// </remarks>
+    internal static class UserAuthentication
+    {
+        internal struct UserCredentials
+        {
+            public readonly string HashedPassword;
+            public readonly string Salt;
+            public readonly int HashingIterations;
+
+            public UserCredentials(string hashedPassword, string salt, int hashingIterations)
+            {
+                HashedPassword = hashedPassword; Salt = salt; HashingIterations = hashingIterations;
+            }
+        }
+
+        /// <summary>
+        /// Generates a new UserCredentials object, including hashed password and salt, for the given user. This 
+        /// method is expected to be used once for each user when they create an account.  The returned UserCredentials 
+        /// object can be stored in the database and compared against for subsequent logins.
+        /// </summary>
+        /// <remarks>This method will take a long time to do password hashing (or it should). </remarks>
+        public static async Task<UserCredentials> GenerateUserCreds(string username, string plaintextPassword)
+        {
+            const int SaltByteLength = 64;
+            const int HashIterations = 100000;
+
+            return await Task.Factory.StartNew<UserCredentials>(() =>
+            {
+                var pbkdf2 = new Rfc2898DeriveBytes(plaintextPassword, SaltByteLength, HashIterations);
+                string hashedPassword = Convert.ToBase64String(pbkdf2.GetBytes(64));
+                string salt = Convert.ToBase64String(pbkdf2.Salt);
+                int iterations = pbkdf2.IterationCount;
+
+                return new UserCredentials(hashedPassword, salt, iterations);
+            });
+        }
+
+        /// <summary>
+        /// Confirms that the hash of the provided plaintext password matches the information contained in the 
+        /// credentials parameter.
+        /// </summary>
+        /// <param name="credentials">The hashed/salted information from the database against which to compare 
+        /// the plaintext password.</param>
+        /// <param name="password">The plaintext password.</param>
+        /// <exception cref="FormatException">The HashedPassword or Salt values in the credentials
+        /// parameter were not of the correct format -- Expected: Base64String </exception>
+        public static async Task<bool> ConfirmUserPassword(UserCredentials credentials, string password)
+        {
+            return await Task.Factory.StartNew<bool>(() =>
+            {
+                var salt = Convert.FromBase64String(credentials.Salt);
+                var passwordHash = Convert.FromBase64String(credentials.HashedPassword);
+                var pbkdf2 = new Rfc2898DeriveBytes(password, salt, credentials.HashingIterations);
+                return pbkdf2.GetBytes(64).SequenceEqual(passwordHash);
+            });
+        }
+    }
+}
